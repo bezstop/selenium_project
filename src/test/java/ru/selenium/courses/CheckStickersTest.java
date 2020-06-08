52da2d9a2b1faaf334fa010bb470a7977e56d62b
bezstop <34750941+bezstop@users.noreply.github.com>
2020-06-08 04:22:49 +0300

    Added new test: check stickers

--- src/test/java/ru/selenium/courses/CheckStickersTest.java
+++ src/test/java/ru/selenium/courses/CheckStickersTest.java
@@ -1,23 +1,36 @@
 package ru.selenium.courses;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import java.util.List;
 
 public class CheckStickersTest extends SetUp {
 
     //Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров
-        public List<WebElement> imageWrapper;
 
-        @Test
-        public void checkStickers() {
-            driver.get("http://localhost/litecart/en/");
-            int productOnPageCount = driver.findElementsByClassName("product").size();
+
+    @Test
+    public void checkStickers() {
+        driver.get("http://localhost/litecart/en/");
+        int productOnPageCount = driver.findElementsByClassName("product").size();
+        for (int i = 0; i < productOnPageCount; i++) {
             int stickerListCount = driver.findElementsByClassName("sticker").size();
-            for (int i = 0; i < productOnPageCount; i++) {
-                System.out.println("Всего элементов:" + productOnPageCount);
-                System.out.println("Всего стикеров:" + stickerListCount);
-                Assert.assertTrue(stickerListCount == productOnPageCount);
-            }
+            Assert.assertTrue("Количество стикеров = " + stickerListCount + " Ожидалось = 1", stickerListCount == 1);
         }
     }
+
+
+    @Test
+    public void checkStickers2() {
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> productOnPageCount = driver.findElements(By.cssSelector(".product"));
+        for (WebElement product: productOnPageCount) {
+            int stickerListCount = product.findElements(By.cssSelector(".sticker")).size();
+            Assert.assertTrue("Количество стикеров = " + stickerListCount + " Ожидалось = 1", stickerListCount == 1);
+        }
+    }
+}
+
+
